671c06cc8b723eb675c31e2d5463ebe7b241c535
Johannes Wolf <mail@johannes-wolf.com>
2022-02-05 16:51:08 +0100

    doom: Enable zen mode

--- modules/doom-emacs.module/link_home/.doom.d/config.el
+++ modules/doom-emacs.module/link_home/.doom.d/config.el
@@ -5,7 +5,7 @@
 
 (setq doom-font (font-spec :family "Source Code Pro" :size 24)
       doom-big-font (font-spec :family "Source Code Pro" :size 26)
-      doom-variable-pitch-font (font-spec :family "Source Serif 4 Display" :size 24))
+      doom-variable-pitch-font (font-spec :family "DejaVu Serif" :size 24))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
