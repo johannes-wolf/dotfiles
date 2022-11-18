83bc7656fc886f4a03ab06293c50e3e8731b706b
Johannes Wolf <mail@johannes-wolf.com>
2022-11-18 23:50:42 +0100

    doom: Switch to non-doom emacs config

---
+++ modules/doom-emacs.disabled/link_home/.doom.d/custom.el
@@ -0,0 +1,34 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-safe-themes
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
+ '(package-selected-packages '(company-maxima))
+ '(safe-local-variable-values
+   '((diff-add-log-use-relative-names . t)
+     (vc-git-annotate-switches . "-w")))
+ '(warning-suppress-log-types
+   '((lsp-mode)
+     (lsp-mode)
+     (org-element-cache)
+     (org-element-cache)
+     (org-element-cache)
+     ((flycheck syntax-checker))
+     (doom-first-buffer-hook)))
+ '(warning-suppress-types
+   '((lsp-mode)
+     (org-element-cache)
+     (org-element-cache)
+     (org-element-cache)
+     ((flycheck syntax-checker))
+     (doom-first-buffer-hook))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
