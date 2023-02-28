0219592fa53b5efa58450edb5b29c9a0a6f141b4
Johannes Wolf <mail@johannes-wolf.com>
2023-02-28 16:30:13 +0100

    emacs: Add fennel mode

--- modules/emacs.module/link_home/.emacs.d/lisp/init-lua.el
+++ modules/emacs.module/link_home/.emacs.d/lisp/init-lua.el
@@ -1,6 +1,11 @@
 (use-package lua-mode
   :ensure t
   :init
-  (setq-default lua-indent-level 3))
+  (setq-default lua-indent-level 3
+		lua-indent-close-paren-align t
+		lua-indent-nested-block-content-align nil))
+
+(use-package fennel-mode
+  :ensure t)
 
 (provide 'init-lua)
