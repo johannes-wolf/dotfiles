ebfd2c832d5de73558f06f298c29c11002ab0b92
Johannes Wolf <git@johannes-wolf.com>
2023-04-17 10:49:50 +0200

    emacs: Use typst-mode.el

--- modules/emacs.module/link_home/.emacs.d/lisp/init-typst.el
+++ modules/emacs.module/link_home/.emacs.d/lisp/init-typst.el
@@ -1,8 +1,4 @@
-;; https://raw.githubusercontent.com/Ziqi-Yang/typst-mode.el/main/typst-mode.el
 (use-package typst-mode
-  :ensure t
-  :quelpa (typst-mode
-	   :fetcher github
-	   :repo "Ziqi-Yang/typst-mode.el"))
+  :straight (typst-mode :type git :host github :repo "Ziqi-Yang/typst-mode.el"))
 
 (provide 'init-typst)
