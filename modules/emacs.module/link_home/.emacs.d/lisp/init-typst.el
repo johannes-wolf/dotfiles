b052eafc517cfaecafb42bbd7d52dbab97396dcd
Johannes Wolf <git@johannes-wolf.com>
2023-04-19 02:12:49 +0200

    emacs: Disable typst-mode for now

--- modules/emacs.module/link_home/.emacs.d/lisp/init-typst.el
+++ modules/emacs.module/link_home/.emacs.d/lisp/init-typst.el
@@ -1,4 +1,11 @@
-(use-package typst-mode
-  :straight (typst-mode :type git :host github :repo "Ziqi-Yang/typst-mode.el"))
+;; typst-mode is full of bugs atm
+;(use-package typst-mode
+;  :straight (typst-mode :type git :host github :repo "Ziqi-Yang/typst-mode.el"))
+
+(define-derived-mode typst-mode
+  prog-mode "Typst"
+  "Major mode for typst.")
+
+(add-to-list 'auto-mode-alist '("\\.typ\\'" . typst-mode))
 
 (provide 'init-typst)
