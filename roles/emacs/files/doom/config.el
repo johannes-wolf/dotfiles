06abfb81a1400efe9b8827088399a705c20d999f
Johannes Wolf <mail@johannes-wolf.com>
2024-11-24 13:48:07 +0100

    emacs: Choose random theme

--- roles/emacs/files/doom/config.el
+++ roles/emacs/files/doom/config.el
@@ -32,12 +32,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;;(setq doom-theme 'doom-1337)
-;;(setq doom-theme 'doom-zenburn
-;;      doom-zenburn-brighter-modeline t)
-;;(setq doom-theme 'doom-laserwave
-;;      doom-laserwave-padded-modeline t)
-(setq doom-theme 'leuven)
+(setq doom-theme-list '(leuven doom-1337 doom-zenburn doom-laserwave doom-oksolar-light))
+(setq doom-theme (nth (random (length doom-theme-list)) doom-theme-list))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
